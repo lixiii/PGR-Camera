b76af3d7ddcb3b2aabd5a0ac478c9fed0eebf4be
lixiii <lieinsteinxi@gmail.com>
2018-08-24 15:51:38 +0800

    initial commit with an example script using PyCapture2

---
+++ example.py
@@ -0,0 +1,39 @@
+# Example file to take images using Point Grey Research camera and use openCV to convert into grey scale
+import PyCapture2
+import numpy as np
+import cv2 
+
+bus = PyCapture2.BusManager()
+cam = PyCapture2.Camera()
+cam.connect(bus.getCameraFromIndex(0))
+
+cam.startCapture()
+
+try:
+    rawImg = cam.retrieveBuffer()
+    
+    bgrImg = rawImg.convert(PyCapture2.PIXEL_FORMAT.BGR)
+    # hack for Python3 save the image first and the load it in openCV because the img.getData() method does not work for 
+    # the converted image
+    bgrImg.save("temp.png".encode(),  PyCapture2.IMAGE_FILE_FORMAT.PNG)
+
+    cvBgrImg = cv2.imread("temp.png")
+    cv2.imshow('BGR image',cvBgrImg)
+
+    # convert to greyscale using opencv library
+    cvGreyImg = cv2.cvtColor(cvBgrImg, cv2.COLOR_RGB2GRAY)
+    cv2.imshow("grey", cvGreyImg)
+
+    # saving images with opencv
+    cv2.imwrite("color.png", cvBgrImg)
+    cv2.imwrite("grey.png", cvGreyImg)
+
+    # saving raw image
+    rawImg.save("raw.png".encode("utf-8"), PyCapture2.IMAGE_FILE_FORMAT.PNG)
+    
+except PyCapture2.Fc2error as fc2Err:
+    print("Error retrieving buffer : ", fc2Err)
+
+cam.stopCapture()
+cam.disconnect()
+cv2.waitKey(0)
