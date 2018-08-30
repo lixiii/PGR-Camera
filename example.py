6a8060b398541466aebabbd1a7f20677f847966b
lixiii <lieinsteinxi@gmail.com>
2018-08-30 16:53:03 +0800

    added comparison

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 # Example using the camera module
 import camera
 import cv2
-import numpy as np
 camera.printNumOfCam()
 camera.init(0)
 camera.capture()
 print("Checking if the image is saturated. Saturation status: " + str( camera.isSaturated( False, True ) ) )
 print("Framerate = " + str( camera.setFramerate(30) ) )
 print("Averaging frames")
-img = camera.captureAverage(10, True)
+img = camera.captureAverage(100, True)
+cv2.imwrite("averaged.png", img)
 camera.close()
